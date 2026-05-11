ae87288c98e8b7b7dfd3a5f27af43151b46b613a
Manoj-Sk21 <manojsasikumar876@gmail.com>
2026-05-11 16:45:03 +0530

    initial commit

---
+++ src/main/java/com/example/jenkins_ci_project/helloworld.java
@@ -0,0 +1,11 @@
+package com.example.jenkins_ci_project;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins CI");
+
+	}
+
+}
